05a4ba5b5f0314a98e5762172c88f7836d1cd1c4
li <395603360@qq.com>
2019-09-10 13:55:11 +0800

    添加css浏览器兼容前缀

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = merge(common, {
         exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'less-loader',
         ]
       }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -44,7 +44,7 @@ module.exports = merge(common, {
             options: { sourceMap: true, },
 
           },
-          // 'postcss-loader',
+          'postcss-loader',
           'less-loader',
         ]
       },
